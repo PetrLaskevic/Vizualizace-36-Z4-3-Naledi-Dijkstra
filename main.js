af0a7dd277197f522068e7355789a01796e40486
PetrLaskevic <139137582+PetrLaskevic@users.noreply.github.com>
2024-12-26 15:26:40 +0100

    Move code starting the maze from mazePicker and file picker to the same place

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import {mazeTextToGraph, prettyPrintGraf} from "./graphGen.js";
 import {DijkstraMazeApp} from "./render.js";
 
+let mazePicker = document.getElementById("mazePicker");
+let mazeAppClassHolderVariable; //the instance of the maze app
 
 function whichLineEnding(source) {
 	var temp = source.indexOf('\n');
@@ -9,8 +11,28 @@ function whichLineEnding(source) {
 	return 'LF' //Linux
 }
 
-let mazePicker = document.getElementById("mazePicker");
-let mazeAppClassHolderVariable; //the instance of the maze app
+function start(unsplitMazeText){
+	let text = "";
+	let lineEnding = whichLineEnding(unsplitMazeText);
+	if(lineEnding == "CRLF"){
+		text = unsplitMazeText.split("\r\n");
+	}else if(lineEnding == "LF"){
+		text = unsplitMazeText.split("\n");
+	}
+
+	let graf = mazeTextToGraph(unsplitMazeText);
+	prettyPrintGraf(graf);
+
+	if(mazeAppClassHolderVariable != undefined){
+		mazeAppClassHolderVariable.zcelaHotovo = true;
+		mazeAppClassHolderVariable.hideMaze();
+	}
+	
+	mazeAppClassHolderVariable = new DijkstraMazeApp(graf);
+	mazeAppClassHolderVariable.renderMaze(text);
+	mazeAppClassHolderVariable.startDijkstra(); //entry point to our actual program
+
+}
 
 mazePicker.addEventListener("change", function(e){
 	let mazeSelected = mazePicker.value;
@@ -47,34 +69,7 @@ mazePicker.addEventListener("change", function(e){
 			}
 		})
    		.then( t => {
-			let text = "";
-			let lineEnding = whichLineEnding(t);
-			if(lineEnding == "CRLF"){
-				text = t.split("\r\n");
-			}else if(lineEnding == "LF"){
-				text = t.split("\n");
-			}
-
-			let graf = mazeTextToGraph(t);
-			prettyPrintGraf(graf);
-
-			//TODO
-			if(mazeAppClassHolderVariable != undefined){
-				mazeAppClassHolderVariable.zcelaHotovo = true;
-				mazeAppClassHolderVariable.hideMaze();
-			}
-			
-            mazeAppClassHolderVariable = new DijkstraMazeApp(graf);
-            mazeAppClassHolderVariable.renderMaze(text);
-            mazeAppClassHolderVariable.startDijkstra(); //entry point to our actual program
-
-            // let grid = document.createElement("responsive-grid");
-            // [grid.rows, grid.columns] = text[0].split(' ').map(Number);
-           
-            // grid.cellStyles = "./visualisation.css";
-            // document.querySelector("main").appendChild(grid);
-            // grid.renderMaze(text);
-
+			start(t);
 		});
 	}
 });
@@ -82,24 +77,12 @@ mazePicker.addEventListener("change", function(e){
 
 //reading and parsing the input into a table to display as well as the correspoding 2D Array
 document.getElementById('inputfile').addEventListener('change', function(event) {
-	console.log(event);
-	let text = "";
-    var fr=new FileReader();
-    fr.onload=function(){
-		let lineEnding = whichLineEnding(fr.result);
-		if(lineEnding == "CRLF"){
-			text = fr.result.split("\r\n");
-		}else if(lineEnding == "LF"){
-			text = fr.result.split("\n");
-		}
-
-		// let graf = mazeTextToGraph(fr.result);
-		// prettyPrintGraf(graf);
-
-		//TODO
-        let grid = document.createElement("responsive-grid");
-        [grid.rows, grid.columns] = text[0].split(' ').map(Number);
-        document.querySelector("main").appendChild(grid);
+    var fr = new FileReader();
+    fr.onload = function(){
+		start(fr.result);
+        // let grid = document.createElement("responsive-grid");
+        // [grid.rows, grid.columns] = text[0].split(' ').map(Number);
+        // document.querySelector("main").appendChild(grid);
     }
     fr.readAsText(this.files[0]);
     document.getElementById("selectedFileLabel").textContent = this.files[0].name;
